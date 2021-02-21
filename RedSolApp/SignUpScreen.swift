9afc40ac095e223e46907e4e97d0a48436e354c3
Yumibyte <ashraigosa@gmail.com>
2021-02-21 10:21:08 -0800

    login outline

---
+++ RedSolApp/SignUpScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpScreen.swift
+//  RedSolApp
+//
+//  Created by Ashley Raigosa on 2/21/21.
+//  Copyright © 2021 Ashley Raigosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUpScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpScreen()
+    }
+}
