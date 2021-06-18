d46b9526cff7bb96aeca35920119851230c5f857
Yumibyte <ashraigosa@gmail.com>
2021-06-17 22:09:45 -0700

    initialized variables, began stacking top part of menu view

--- RedSolApp/RedSolApp/TabPages/ProfileViewMenu.swift
+++ RedSolApp/RedSolApp/TabPages/ProfileViewMenu.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ProfileViewMenu: View {
+    
+    var firstName: String = "Nombre"
+    var lastName: String = "Apellido"
+    var email: String = "correo@correo.com"
+    var blueColor: Color = Color(red: 82 / 255, green: 130 / 255, blue: 240 / 255)
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello World!")
+        ZStack {
+            Rectangle()
+                .frame(width: .infinity, height: 200)
+                .foregroundColor(blueColor)
+            
+            VStack(alignment: .center) {
+                
+                Image(systemName: "person.crop.circle")
+                    .font(.system(size: 150))
+                    .foregroundColor(.black)        // change to white later
+                
+                Text(firstName + " " + lastName).bold()
+                Text(email)
+            }
+            
+
         }
         .offset(x: 10, y: -300)
         .frame(maxWidth: .infinity, alignment: .leading)
