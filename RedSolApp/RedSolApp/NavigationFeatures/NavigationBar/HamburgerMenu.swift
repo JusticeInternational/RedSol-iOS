e8bb2166195ac85e4667a0dfcb1d8a85716a52ba
Yumibyte <ashraigosa@gmail.com>
2021-06-28 00:12:03 -0700

    smooth navigation but odd warning

--- RedSolApp/RedSolApp/NavigationFeatures/NavigationBar/HamburgerMenu.swift
+++ RedSolApp/RedSolApp/NavigationFeatures/NavigationBar/HamburgerMenu.swift
@@ -12,7 +12,9 @@ struct HamburgerMenu: View {
     // Navigation View button variables
     @State var isProfileViewActive = false
     @State var isConfigurationViewActive = false
-    @State var isContactUs
+    @State var isContactUsViewActive = false
+    @State var isSuggestResourcesViewActive = false
+    @State var isLoginViewActive = false
     
     // profile view variables
     var firstName: String = "Nombre"
@@ -27,114 +29,145 @@ struct HamburgerMenu: View {
     }
     
     func configuration() {
+        isConfigurationViewActive.toggle()
         print("configuration")
     }
     
     func contactUs() {
+        isContactUsViewActive.toggle()
         print("contact us")
     }
     
     func suggestResources() {
+        isSuggestResourcesViewActive.toggle()
         print("suggest resources")
     }
     
     func login() {
+        isLoginViewActive.toggle()
         print("login")
     }
     
     
     var body: some View {
         
-        VStack(alignment: .leading) {
-            
-//            HStack {
-//                Image(systemName: "person.fill")
-//                    .foregroundColor(customBlue)
-//                    .imageScale(.large)
-//                Text("Mi perfil")
-//                    .foregroundColor(.gray)
-//                    .font(.headline)
-//            }
-//            .padding(.top, 100)
-            HStack {
+//        NavigationView {
+            VStack(alignment: .leading) {
                 
-                Button(action: { myProfile() } ) {
-                    Image(systemName: "person.fill")
-                        .foregroundColor(customBlue)
-                        .imageScale(.large)
-                    Text("My Profil")
-                        .foregroundColor(.gray)
-                        .font(.headline)
-                    
-                    
-                
-                }
-                NavigationLink(destination: ProfileView(), isActive: $isProfileViewActive) {
-                    EmptyView()
-                }
-            }
-            .padding(.top, 100)
-            
-            HStack {
-                
-                Button(action: { configuration() }) {
-                    Image(systemName: "slider.horizontal.3")
-                        .foregroundColor(customBlue)
-                        .imageScale(.large)
-                    Text("Configuración")
-                        .foregroundColor(.gray)
-                        .font(.headline)
-                }
-            }
-            .padding(.top, 30)
-
-            HStack {
-                
-                Button(action: { contactUs() } ) {
-                    Image(systemName: "envelope.fill")
-                        .foregroundColor(customBlue)
-                        .imageScale(.large)
-                    Text("Contáctanos")
-                        .foregroundColor(.gray)
-                        .font(.headline)
-                }
-            }
-            .padding(.top, 30)
-            
-            HStack {
-                
-                Button(action: { suggestResources() }) {
-                    Image(systemName: "flag.fill")
-                        .foregroundColor(customBlue)
-                        .imageScale(.large)
-                    Text("Sugerir Recursos")
-                        .foregroundColor(.gray)
-                        .font(.headline)
-                }
-            }
-            .padding(.top, 30)
-            
-            Button(action: { login() }) {
+    //            HStack {
+    //                Image(systemName: "person.fill")
+    //                    .foregroundColor(customBlue)
+    //                    .imageScale(.large)
+    //                Text("Mi perfil")
+    //                    .foregroundColor(.gray)
+    //                    .font(.headline)
+    //            }
+    //            .padding(.top, 100)
                 HStack {
-                    Image(systemName: "arrow.right.square.fill")
-                        .foregroundColor(customBlue)
-                        .imageScale(.large)
-                    Text("Iniciar Sesión")
-                        .foregroundColor(.gray)
-                        .font(.headline)
+                    
+                    Button(action: { myProfile() } ) {
+                        Image(systemName: "person.fill")
+                            .foregroundColor(customBlue)
+                            .imageScale(.large)
+                        Text("My Profil")
+                            .foregroundColor(.gray)
+                            .font(.headline)
+                        
+                        
+                    
+                    }
+                    NavigationLink(destination: ProfileView(), isActive: $isProfileViewActive) {
+                        EmptyView()
+                    }
+
                 }
-            }
-            
-            .offset(y: 500)
+                .padding(.top, 100)
+                
+                
+                HStack {
+                    
+                    Button(action: { configuration() }) {
+                        Image(systemName: "slider.horizontal.3")
+                            .foregroundColor(customBlue)
+                            .imageScale(.large)
+                        Text("Configuración")
+                            .foregroundColor(.gray)
+                            .font(.headline)
+                    }
+                    
+                    NavigationLink(destination: ConfigurationView(), isActive: $isConfigurationViewActive) {
+                        EmptyView()
+                    }
+                }
+                .padding(.top, 30)
 
                 
+                HStack {
+                    
+                    Button(action: { contactUs() } ) {
+                        Image(systemName: "envelope.fill")
+                            .foregroundColor(customBlue)
+                            .imageScale(.large)
+                        Text("Contáctanos")
+                            .foregroundColor(.gray)
+                            .font(.headline)
+                    }
+                    
+                    NavigationLink(destination: ContactUsView(), isActive: $isContactUsViewActive) {
+                        EmptyView()
+                    }
+                }
+                .padding(.top, 30)
+                
+                
+                HStack {
+                    
+                    Button(action: { suggestResources() }) {
+                        Image(systemName: "flag.fill")
+                            .foregroundColor(customBlue)
+                            .imageScale(.large)
+                        Text("Sugerir Recursos")
+                            .foregroundColor(.gray)
+                            .font(.headline)
+                    }
+                    
+                    NavigationLink(destination: SuggestResourcesView(), isActive: $isSuggestResourcesViewActive) {
+                        EmptyView()
+                    }
+                }
+                .padding(.top, 30)
+                
+                Button(action: { login() }) {
+                    HStack {
+                        Image(systemName: "arrow.right.square.fill")
+                            .foregroundColor(customBlue)
+                            .imageScale(.large)
+                        Text("Iniciar Sesión")
+                            .foregroundColor(.gray)
+                            .font(.headline)
+                    }
+                    
+                    NavigationLink(destination: LoginView(), isActive: $isLoginViewActive) {
+                        EmptyView()
+                    }
+                }
+                
+                .offset(y: 500)
+
+                    
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .edgesIgnoringSafeArea(.all)
             
-            Spacer()
-        }
-        .padding()
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .edgesIgnoringSafeArea(.all)
+//        }
+        .navigationViewStyle((StackNavigationViewStyle()))
+
+        
     }
+    
 }
 
 struct HamburgerMenu_Previews: PreviewProvider {
