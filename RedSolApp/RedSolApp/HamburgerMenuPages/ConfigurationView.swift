dff930142f344a30f15cfa3bc73460dec8732f31
Yumibyte <ashraigosa@gmail.com>
2021-06-28 23:32:40 -0700

    created design of contact us page

--- RedSolApp/RedSolApp/HamburgerMenuPages/ConfigurationView.swift
+++ RedSolApp/RedSolApp/HamburgerMenuPages/ConfigurationView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ConfigurationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text("Cambio de contraseña")
     }
 }
 
--- RedSolApp/RedSolApp/HamburgerMenuPages/ContactUsView.swift
+++ RedSolApp/RedSolApp/HamburgerMenuPages/ContactUsView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct ContactUsView: View {
+    
+    @State var customBlue = Color(red: 82 / 256, green: 130 / 256, blue: 240 / 256, opacity: 1.0)
+    
+    // contact buttons
+    
+    func llamar() {
+        print("llamar")
+    }
+    
+    func email() {
+        print("email")
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 60) {
+            VStack(spacing: 30) {
+                        Text("¿Cómo podemos ayudar?")
+                            .bold()
+                            .foregroundColor(customBlue)
+                            .font(.custom("Roboto-Bold", size: 30))
+                        Text("De parte del equipo de Red Solidaria nos gustaría conocer ccomo ayudarte, puedes dejar aquí cualquier comentario, duda o sugerencia.")
+                            .font(.custom("Roboto-Regular", size: 18))
+                            
+                            .padding(30)
+                    }
+                    
+            HStack(spacing: 100) {
+                HStack {
+                    
+                    Button(action: { llamar() }) {
+                        
+                        Text("Llamar")
+                        Image(systemName: "phone.fill")
+                    }
+                    
+                }
+                
+                HStack {
+                    
+                    Button(action: { email() }) {
+                        Text("Correo")
+                        Image(systemName: "envelope.fill")
+                    }
+                    
+                }
+            }
+                
+
+        }
+        .offset(y: -150)
+        
+        
     }
 }
 
